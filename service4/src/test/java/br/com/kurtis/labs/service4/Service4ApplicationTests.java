d921e992feffa5a6d3d5469b986991fef624ed97
Tiago Angelo <tiago.angelo@sensedia.com>
2018-07-10 23:57:41 -0300

    Environment variables for unit tests

--- service4/src/test/java/br/com/kurtis/labs/service4/Service4ApplicationTests.java
+++ service4/src/test/java/br/com/kurtis/labs/service4/Service4ApplicationTests.java
@@ -9,6 +9,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Service4ApplicationTests {
 
+  static {
+    System.setProperty("SERVICE1_ADDRESS", "");
+    System.setProperty("SERVICE2_ADDRESS", "");
+    System.setProperty("SERVICE3_ADDRESS", "");
+    System.setProperty("SERVICE4_ADDRESS", "");
+  }
+
   @Test
   public void contextLoads() {
   }
