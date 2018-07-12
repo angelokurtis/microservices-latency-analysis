a4536b1267a1b91670f92326f099cb9b1313dd9b
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2018-07-11 23:25:19 -0300

    jaeger env name

--- service1/src/main/java/br/com/kurtis/labs/service1/Service1Application.java
+++ service1/src/main/java/br/com/kurtis/labs/service1/Service1Application.java
@@ -1,11 +1,13 @@
 package br.com.kurtis.labs.service1;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Log4j2
 @SpringBootApplication
 public class Service1Application {
 
@@ -18,6 +20,7 @@ public class Service1Application {
 
   @Bean
   WebClient.Builder webClientBuilder() {
+    log.info("service 2 address => " + this.service2Address);
     return WebClient
         .builder()
         .baseUrl(service2Address);
